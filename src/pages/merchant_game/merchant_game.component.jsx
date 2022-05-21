ed78d5a2697a1b9a02f6a02e9d74084f5bda8ccd
Yuriy <yuriy.plakhin@gmail.com>
2022-05-21 21:49:33 +0600

    Main game class created, most of methods implemented, while loop for purchase implemented etc.

--- src/pages/merchant_game/merchant_game.component.jsx
+++ src/pages/merchant_game/merchant_game.component.jsx
@@ -1,12 +1,11 @@
 import React from "react";
+
 import "./merchant_game.styles.scss";
 
 const MerchantGame = () => {
-  // SETTINGS
-
   class Randomizer {
     static generateRandomNumber(min, max) {
-      let random = Math.round(Math.random() * (max - min + 1) + min);
+      let random = Math.round(Math.random() * (max - min) + min);
       return random;
     }
 
@@ -16,59 +15,156 @@ const MerchantGame = () => {
   }
 
   class Merchant {
-    #min_good_price = 10;
+    #MIN_STARTING_MONEY = 10;
+    #MAX_STARTING_MONEY = 100;
+    #DEFAULT_WEIGHT_LIMIT = 30;
 
-    constructor(weightLimit, speed, money) {
-      this.weightLimit = weightLimit;
-      this.speed = speed;
-      this.money = money;
-    }
-
-    isMoneyEnough(nextGoodPrice = this.#min_good_price) {
-      if (this.money >= 0 && this.money >= nextGoodPrice) return true;
-      return false;
+    constructor() {
+      this.weightLimit = this.#DEFAULT_WEIGHT_LIMIT;
+      this.currentLoad = 0;
+      this.speed = 0;
+      this.money = this.earnStartingMoney();
+      this.goodsInCart = [];
     }
 
     sellAllGoods(goodsPricesList) {
       this.money = goodsPricesList.reduce((a, b) => a + b, this.money);
     }
+
+    measureSpeed() {
+      this.speed = Randomizer.generateRandomNumber(1, 5);
+    }
+
+    earnStartingMoney() {
+      return Randomizer.generateRandomNumber(
+        this.#MIN_STARTING_MONEY,
+        this.#MAX_STARTING_MONEY
+      );
+    }
+
+    setCartSpeed() {
+      this.speed = Randomizer.generateRandomNumber(1, 5);
+    }
+
+    buyGood() {
+      const good = new Good();
+
+      this.money = this.money - good.buyPrice;
+      this.currentLoad = this.currentLoad + good.weight;
+      this.goodsInCart.push(good);
+    }
   }
   class Good {
-    #qualityList = {
-      normal: 1.2,
-      "slightly spoiled": 0.95,
-      "half spoiled": 0.55,
-      "almost fully spoiled": 0.25,
-      spoiled: 0.1,
+    #QUALITY_LIST = {
+      Normal: 1.2,
+      "Slightly spoiled": 0.95,
+      "Half spoiled": 0.55,
+      "Almost fully spoiled": 0.25,
+      Spoiled: 0.1,
     };
 
-    #product_names = ["Мясо", "Сухофрукты", "Зерно", "Мука", "Ткани", "Краска"];
+    #PRODUCTS = [
+      { name: "Мясо", weight: 10, buyPrice: 20 },
+      { name: "Сухофрукты", weight: 2, buyPrice: 16 },
+      { name: "Зерно", weight: 20, buyPrice: 10 },
+      { name: "Мука", weight: 5, buyPrice: 4 },
+      { name: "Ткани", weight: 7, buyPrice: 30 },
+      { name: "Краска", weight: 9, buyPrice: 36 },
+    ];
 
-    constructor(goodWeight, quality, buyPrice) {
-      this.name = this.chooseProduct();
-      this.goodWeight = goodWeight;
-      this.quality = quality;
-      this.buyPrice = buyPrice;
+    constructor() {
+      const good = this.chooseProduct();
+
+      this.name = good.name;
+      this.quality = this.#QUALITY_LIST.Normal;
+      this.weight = good.weight;
+      this.buyPrice = good.buyPrice;
     }
 
     chooseProduct() {
       return Randomizer.getRandomItem(
-        this.#product_names,
+        this.#PRODUCTS,
         0,
-        this.#product_names.length - 1
+        this.#PRODUCTS.length - 1
       );
     }
 
     calculateSellPrice() {
-      this.buyPrice = this.buyPrice * this.quality;
+      this.buyPrice *= this.quality;
     }
+
+    getLowestRise(field) {
+      function sortFromLowest(a, b) {
+        return a[field] - b[field];
+      }
+
+      this.#PRODUCTS.sort(sortFromLowest);
+      return this.#PRODUCTS[0][field];
+    }
+
+    // constructor() {
+    //   this.name = "";
+    //   this.quality = this.#QUALITY_LIST.Normal;
+    //   this.weight = 0;
+    //   this.buyPrice = 0;
+
+    //   this.setProduct();
+    // }
+
+    // async setProduct() {
+    //   try {
+    //     const product = await this.chooseProduct();
+
+    //     this.name = product.name;
+    //     this.quality = this.#QUALITY_LIST.Normal;
+    //     this.weight = product.weight;
+    //     this.buyPrice = product.buyPrice;
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // }
+
+    // fetchProducts() {
+    //   const products = [
+    //     { name: "Мясо", weight: 10, buyPrice: 20 },
+    //     { name: "Сухофрукты", weight: 2, buyPrice: 16 },
+    //     { name: "Зерно", weight: 20, buyPrice: 10 },
+    //     { name: "Мука", weight: 5, buyPrice: 4 },
+    //     { name: "Ткани", weight: 7, buyPrice: 30 },
+    //     { name: "Краска", weight: 9, buyPrice: 36 },
+    //   ];
+
+    //   return new Promise((resolve) =>
+    //     setTimeout(() => resolve(products), 2000)
+    //   ).then((resolved) => resolved);
+    // }
+
+    // async chooseProduct() {
+    //   const products = await this.fetchProducts();
+
+    //   return Randomizer.getRandomItem(products, 0, products.length - 1);
+    // }
+
+    // calculateSellPrice() {
+    //   this.buyPrice *= this.quality;
+    // }
+
+    // async getLowestRise(field) {
+    //   function sortFromLowest(a, b) {
+    //     return a[field] - b[field];
+    //   }
+
+    //   const products = await this.fetchProducts();
+    //   products.sort(sortFromLowest);
+    //   return products[0][field];
+    // }
   }
 
   class City {
-    #min_distance = 50;
-    #max_distance = 100;
+    #MIN_DISTANCE = 50;
+    #MAX_DISTANCE = 100;
 
-    #city_names = [
+    #CITY_NAMES = [
       "Алматы",
       "Астана",
       "Актау",
@@ -79,15 +175,15 @@ const MerchantGame = () => {
     ];
 
     constructor() {
-      this.name = Randomizer.getRandomItem(this.#city_names, 0, 6);
+      this.name = Randomizer.getRandomItem(this.#CITY_NAMES, 0, 6);
       this.distance = Randomizer.generateRandomNumber(
-        this.#min_distance,
-        this.#max_distance
+        this.#MIN_DISTANCE,
+        this.#MAX_DISTANCE
       );
     }
 
     ChooseCity() {
-      Randomizer.getRandomItem(this.#city_names, 0, 6);
+      Randomizer.getRandomItem(this.#CITY_NAMES, 0, 6);
     }
 
     GenerateDistance() {
@@ -95,7 +191,7 @@ const MerchantGame = () => {
     }
   }
   class Event {
-    #events = [
+    #EVENTS = [
       "Обычный День",
       "Дождь",
       "Ровная дорога",
@@ -112,29 +208,51 @@ const MerchantGame = () => {
     }
 
     ChooseEvent() {
-      Randomizer.getRandomItem(this.#events, 0, 6);
+      Randomizer.getRandomItem(this.#EVENTS, 0, 6);
     }
   }
 
   class Engine {
     constructor() {
       this.player = new Merchant();
+      this.destinationCity = new City();
+
+      const { price: lowestPrice, weight: lowestWeight } =
+        this.getLowestValues();
+
+      while (
+        this.player.money >= lowestPrice &&
+        this.player.currentLoad < this.player.weightLimit &&
+        this.player.currentLoad >= lowestWeight
+      ) {
+        this.player.buyGood();
+      }
+    }
+
+    getLowestValues() {
+      const good = new Good();
+
+      const lowest = {
+        price: good.getLowestRise("buyPrice"),
+        weight: good.getLowestRise("weight"),
+      };
+
+      return lowest;
     }
   }
 
-  let c = new City();
-  let e = new Event();
-  let b = 10;
-  let list = [5, 10, 15];
-  let test = () => {
-    b = list.reduce((a, c) => a + c, b);
-    console.log(b);
-  };
+  let good = new Engine();
 
   return (
     <div onClick={() => test()}>
       <div className="start--and__settings">
-        <form action="submit">
+        <form
+          action="submit"
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log("Hi");
+          }}
+        >
           <input className="start--btn" type="submit" value="Start" />
           <div className="settings">
             <input type="number" placeholder="Грузоподъемность телеги" />
